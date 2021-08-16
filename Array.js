5defc012599509b168aba57e7ce67f08404d6712
Ajith <ajith@calibraint.com>
2021-08-16 12:05:45 +0530

    some text added

--- Array.js
+++ Array.js
@@ -1,70 +1,72 @@
-let a,b,s,r,t,x,y,z,w;
-a=5;
-b=10;
+//this is javascript comments
+'use strict';
+ //strict mode enabled
+ //arithmatic operator
+let arith = (x,y)=>{
+       return { first : x+y,
+        second : x == y,
+        third :  x & y //bitwise 10-1010 ,12-1100
+       }
+};
+var result = arith (10,12);
+console.log(result.first);
+console.log(result.second);
+console.log(result.third);
+console.log(typeof(10));//typeof
 
-x=10+5;//arithmatic operator 
-console.log(s);
-y=20-8;//arithmatic operator 
-z=-x //unary minus
-console.log(z);
-
-w=+y; //unary positive //1100
-console.log(w);
-
-s=10//1010  
-r=y**s; //multiplicative operator
-console.log("the multiplicative operator " +r);
-
- t= r>y ? "r greater" : "y greater";//ternary operator
- console.log("the ternary operator " +t);
-
- s+=y;//assignment operator
- while(y<15) //while loop
- {
-    console.log(y);
-     y++;
- }
-
-// break and continue
-for(let u=2;u<6;u++){
-    console.log("the value of u is",u);
-
-   if (u==4){
-    console.log("entered in if");
-    //boolean operators
-       continue  ;
-   }
-}
-//assignment operator
-//switch  & break
-console.log(" the AND operator " +(y&s));//bitwise AND //y&s
-for(let i = 2 ; i<6;i++){
-    switch(i){
-        case 1:
-            console.log("the number is 1");
-            break; //break statement
-        case 3:
-            console.log("the numbers is 5");
-            break;
-        case 5:
-            console.log("the number is 5");
-            break;
-        default :
-             console.log("the number is not in the case");
+//if statement
+let c = (a)=> {
+    if (a >20){
+        return "valid";
     }
-    
+    else if(a=20){
+        return "same";
+    }
+    else {
+        return "invalid";
+    }
 }
+console.log(c(22));
 
-console.log(s==y);//relational operator
-//bitwise AND operator
-if(a==5 ^ b==12){
-    console.log("s = 10 and a == 5");
+//do - while 
+
+let val="";
+let doWh = (inc)=>{
+do {
+  val+= "\n The number is  " + inc ;
+  inc++;
+
+}while(inc<16);
+};
+doWh(12);
+console.log(val);
+
+//for statement ,break and continue
+let ne="";
+let fo = (dig)=>{
+    for(;dig<12;dig++){
+     if(dig == 7){
+         continue;
+     }
+     if(dig == 9){
+     break;
+     }
+     ne+=   "  \n the dig is " + dig;
+
+    };
+
 }
-else{
-    console .log("declared value is wrong");
-}
-//for - in
-let i ={name:"name",degree:"b.e",branch:"cse"};
-for(let arr in i){
-  console.log(i[arr]);
-}
+fo(5);
+console.log(ne);
+
+//switch  
+
+let swi = (s) =>{
+    switch (s){
+        case 1: return "case 1";
+        case 2 : return "case 2";
+        case 3 : return "case 3";
+        default : return " try again";
+    }
+};
+console .log(swi(6));
