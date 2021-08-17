672732df222e759e20acea94aaa337be3cd782a7
Ajith <ajith@calibraint.com>
2021-08-17 20:34:49 +0530

    committed

---
+++ objects.js
@@ -0,0 +1,6 @@
+// Create an object:
+const car = {type:"honda", model:"500", color:"white"};
+
+// Display some data from the object:
+console.log("The car type is " + car.type + " and model is " + car.model);
+
