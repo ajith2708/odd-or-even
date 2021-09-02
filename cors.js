de2d5aea738cab87f75fa515fbef90d4dd152465
Ajith <ajith@calibraint.com>
2021-09-03 00:44:48 +0530

    cors

---
+++ cors.js
@@ -0,0 +1,24 @@
+//cors
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.get('/:name', (req, res) => {
+    let name = req.params.name;
+
+    res.json({
+        message: `Hello ${name}`
+    });
+});
+
+app.listen(2020, () => {
+    console.log('server is listening on port 2020');
+});
